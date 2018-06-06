10e795184a4e3a78fc8d18e824eab78c2f83aaa5
Scott Schindler <scottschindler29@gmail.com>
2018-06-06 04:02:34 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,6 @@
-def say_hello 
-  
-  
-  
-  
-  def greeting(name, language="Ruby")
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+def say_hello(name="ruby programmer")
+  puts "Hello, #{name}!"
 end
+  
+  
+  
